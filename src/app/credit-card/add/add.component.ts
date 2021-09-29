06556fc25a96b4dc14e3f0a44f1df3edff9d3908
Christian Thing Knudsen <ct-1000@hotmail.com>
2021-09-29 12:12:58 +0200

    Testcode added

--- src/app/credit-card/add/add.component.ts
+++ src/app/credit-card/add/add.component.ts
@@ -8,5 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class CreditCardAddComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log('Credit card add oninit!');
+  }
 }
--- src/app/transaction/overview/overview.component.ts
+++ src/app/transaction/overview/overview.component.ts
@@ -8,5 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class TransactionOverviewComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log('Transaction overview on init!');
+  }
 }
