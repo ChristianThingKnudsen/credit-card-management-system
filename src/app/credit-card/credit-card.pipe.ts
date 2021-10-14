613e0fa9a2548fee80c29301013d8144f2ac3286
Christian Thing Knudsen <ct-1000@hotmail.com>
2021-10-14 20:11:03 +0200

    Custom pipe for expiration date added

--- src/app/credit-card/credit-card.pipe.ts
+++ src/app/credit-card/credit-card.pipe.ts
@@ -2,11 +2,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { CreditCard } from './credit-card.type';
 
 @Pipe({
-  name: 'expiration-date',
+  name: 'expiration',
 })
 export class CreditCardPipe implements PipeTransform {
-  transform(creditCard: CreditCard): String | null {
-    //TODO return total expiration data
-    return '';
+  transform(creditCard: CreditCard): Date | null {
+    return new Date(
+      Number(creditCard.expiration_date_year) - 1,
+      Number(creditCard.expiration_date_month) - 1
+    );
   }
 }
